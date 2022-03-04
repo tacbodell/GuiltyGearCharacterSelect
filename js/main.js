2c949ec88b5c6fb4b1f856708898d66b7793965b
Bryson Coons <coonsbrysona@gmail.com>
2022-03-04 18:10:06 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+document.getElementById('jackoButton').onclick = selectJacko // Character Select Bar Event Listeners
+document.getElementById('nagoriyukiButton').onclick = selectNagoriyuki
+document.getElementById('milliaButton').onclick = selectMillia
+document.getElementById('chippButton').onclick = selectChipp
+document.getElementById('solButton').onclick = selectSol
+document.getElementById('kyButton').onclick = selectKy
+document.getElementById('mayButton').onclick = selectMay
+document.getElementById('zatoButton').onclick = selectZato
+document.getElementById('inoButton').onclick = selectIno
+document.getElementById('happyButton').onclick = selectHappy
+document.getElementById('baikenButton').onclick = selectBaiken
+document.getElementById('anjiButton').onclick = selectAnji
+document.getElementById('leoButton').onclick = selectLeo
+document.getElementById('faustButton').onclick = selectFaust
+document.getElementById('axlButton').onclick = selectAxl
+document.getElementById('potemkinButton').onclick = selectPotemkin
+document.getElementById('ramlethalButton').onclick = selectRamlethal
+document.getElementById('giovannaButton').onclick = selectGiovanna
+document.getElementById('currentCharacterDisplay').onmouseover = playGiovanna
+document.getElementById('goldlewisButton').onclick = selectGoldlewis
+
+// Character Select Bar Functions
+function selectJacko() {
+    document.getElementById('currentCharacterDisplay').src = 'images/jackoFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'JACK-O VALENTINE'
+}
+function selectNagoriyuki() {
+    document.getElementById('currentCharacterDisplay').src = 'images/nagoriyukiFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'NAGORIYUKI'
+}
+function selectMillia() {
+    document.getElementById('currentCharacterDisplay').src = 'images/milliaFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'MILLIA RAGE'
+}
+function selectChipp() {
+    document.getElementById('currentCharacterDisplay').src = 'images/chippFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'CHIPP ZANUFF'
+}
+function selectSol() {
+    document.getElementById('currentCharacterDisplay').src = 'images/solFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'SOL BADGUY'
+}
+function selectKy() {
+    document.getElementById('currentCharacterDisplay').src = 'images/kyFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'KY KISKE'
+}
+function selectMay() {
+    document.getElementById('currentCharacterDisplay').src = 'images/mayFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'MAY'
+}
+function selectZato() {
+    document.getElementById('currentCharacterDisplay').src = 'images/zatoFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'ZATO-1'
+}
+function selectIno() {
+    document.getElementById('currentCharacterDisplay').src = 'images/inoFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'I-NO'
+}
+function selectHappy() {
+    document.getElementById('currentCharacterDisplay').src = 'images/happyFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'HAPPY CHAOS'
+}
+function selectBaiken() {
+    document.getElementById('currentCharacterDisplay').src = 'images/baikenFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'BAIKEN'
+}
+function selectAnji() {
+    document.getElementById('currentCharacterDisplay').src = 'images/anjiFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'ANJI MITO'
+}
+function selectLeo() {
+    document.getElementById('currentCharacterDisplay').src = 'images/leoFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'LEO WHITEFANG'
+}
+function selectFaust() {
+    document.getElementById('currentCharacterDisplay').src = 'images/faustFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'FAUST'
+}
+function selectAxl() {
+    document.getElementById('currentCharacterDisplay').src = 'images/axlFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'AXL LOW'
+}
+function selectPotemkin() {
+    document.getElementById('currentCharacterDisplay').src = 'images/potemkinFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'POTEMKIN'
+}
+function selectRamlethal() {
+    document.getElementById('currentCharacterDisplay').src = 'images/ramlethalFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'RAMLETHAL VALENTINE'
+}
+function selectGiovanna() {
+    document.getElementById('currentCharacterDisplay').src = 'images/giovannaFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'GIOVANNA'
+}
+function selectGoldlewis() {
+    document.getElementById('currentCharacterDisplay').src = 'images/goldlewisFB.png'
+    document.getElementById('currentCharacterName').innerHTML = 'GOLDLEWIS DICKINSON'
+}
+
+
+
+
